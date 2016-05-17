eba116f9b120e1db7c2522fbceae3de631017456
Ratonhnaketon <diego@gallego.com.br>
2016-05-17 20:46:59 -0300

    Add files via upload

---
+++ motor.py
@@ -0,0 +1,27 @@
+
+class motor(object):
+	def __init__(self, graph):
+		self.matrix = motor.graphToMatriz(graph)
+
+	def genGraph():
+		pass
+
+	def graphToMatriz(graph):
+		pass
+
+	# Adiciona um 'peso' em um dos nos da estrutura aleatoriamente
+	def addForce(self):
+		pass
+
+	# Soluciona a estrutura
+	# 1º: Recebe uma matriz e verifica se e isostatica
+	# 2º: Descobre o valor das forcas em cada no e verifica
+	# se nao e maior... terminar
+	def solution(self):
+		pass
+
+	def firstStep(self):
+		pass
+
+	def secondStep(self):
+		pass
